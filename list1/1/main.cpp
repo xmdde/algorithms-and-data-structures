6d3b8773a271428202230fc03b1ba0ee79c06236
justynaziemichod <268431@student.pwr.edu.pl>
2023-03-11 13:29:16 +0100

    finished stack implementation

--- list1/1/main.cpp
+++ list1/1/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-#define MAX_SIZE 5
+#define MAX_SIZE 1000
 
 class Queue {
 private:
@@ -47,17 +47,74 @@ public:
         delete [] arr;
     }
 
-    void help() {
+    void help() { //move to tests
         cout << "front: " << front << " rear: " << rear << '\n';
     }
 };
 
 class Stack {
+private:
+    int* arr;
+    int top;
+    int size;
+public:
+    Stack() {
+        this->top = 0;
+        this->size = MAX_SIZE;
+        this->arr = new int[size];
+    }
 
+    void addElement(int val) {
+        if (top == size) {
+            cout << "Stack overflow! ;D\n";
+        }
+        else {
+            arr[top] = val;
+            top++;
+        }
+    }
+
+    int pop() {
+        if (top == 0) {
+            cerr << "pop: Empty stack\n";
+            exit(EXIT_FAILURE);
+        }
+        else {
+            top--;
+            return arr[top];
+        }
+    }
+
+    ~Stack() {
+        delete [] arr;
+    }
 };
 
 int main() {
-    Queue q;
+    srand(time(nullptr));
+    Queue* q = new Queue;
+    Stack* s = new Stack;
+    cout << "Adding 100 elements to the queue.\n";
+    int element;
+    for (int i = 0; i < 100; i++) {
+        element = rand()%1000;
+        cout << element << "\n";
+        q->enqueue(element);
+    }
+    cout << "Dequeuing: \n";
+    for (int i = 0; i < 100; i++) {
+        cout << q->dequeue() << '\n';
+    }
 
+    cout << "\nAdding 100 elements to the stack.\n";
+    for (int i = 0; i < 100; i++) {
+        element = rand()%1000;
+        cout << element << "\n";
+        s->addElement(element);
+    }
+    cout << "Taking elements from the stack: \n";
+    for (int i = 0; i < 100; i++) {
+        cout << s->pop() << '\n';
+    }
     return 0;
 }
